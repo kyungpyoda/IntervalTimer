54c85cbdb5e1f06d3daa62b1f8c999712dfbc370
홍경표 <qorghdl@naver.com>
2022-01-17 17:41:19 +0900

    Init Project
    - watchOS 8.0+
    - SwiftUI

---
+++ IntervalTimer/IntervalTimer WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IntervalTimer WatchKit Extension
+//
+//  Created by 홍경표 on 2022/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IntervalTimer/IntervalTimer WatchKit Extension/IntervalTimerApp.swift
@@ -0,0 +1,19 @@
+//
+//  IntervalTimerApp.swift
+//  IntervalTimer WatchKit Extension
+//
+//  Created by 홍경표 on 2022/01/17.
+//
+
+import SwiftUI
+
+@main
+struct IntervalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
